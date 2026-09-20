da7a93e951a58ac6e22c2c8699e708a8cfe79755
ag579377-lgtm <ag579377@gmail.com>
2026-09-20 12:32:10 +0200

    fixed regex error

--- src/server.js
+++ src/server.js
@@ -345,7 +345,7 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.get("*", (req, res) =>
+app.get(/(.*)/, (req, res) =>
   res.sendFile(path.join(__dirname, "../public/index.html"))
 );
 
